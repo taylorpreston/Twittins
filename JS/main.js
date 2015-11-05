b2ccd46708517530c1d1b1d79c64410dbe3c9b1e
Taylor Preston <taylor.preston.music@gmail.com>
2015-11-05 13:38:15 -0600

    First Commit

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+
+// *** Collections ***
+PublicCollection = Backbone.Collection.extend({
+});
+
+FollowingCollection = Backbone.Collection.extend({
+});
+
+
+
+// *** VIEWS ***
+TimelineView = Backbone.View.extend({
+});
+
+LoginView = Backbone.View.extend({
+});
+
+RegisterView = Backbone.View.extend({
+});
+
+ProfileView = Backbone.View.extend({
+});
